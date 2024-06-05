daa3996c9b12bf72f23dc61549dc92415cace620
Paul Nong-Laolam <pnong-laolam@espec.com>
2024-06-05 11:46:33 -0400

    update version number and required contents

--- setup.py
+++ setup.py
@@ -3,23 +3,30 @@
 from setuptools import setup
 
 def readme():
-    '''pull iin the readme file for the long description'''
+    '''pull in the readme file for the long description'''
     with open('README.md') as rfile:
         return rfile.read()
 
 setup(
-    name='chamberconnectlibrary',
-    version='2.3.1',
-    description='A library for interfacing with Espec North America chambers',
-    long_description=readme(),
+    name='chamberconnectlibrary',       # should match the pkg folder
+    version='2.0.0',                    # important for future updates
+    description='A library for interfacing with Espec North America chambers via Python 3.6+',
+    long_description=readme(),          # loads the README.md file 
     url='https://github.com/EspecNorthAmerica/ChamberConnectLibrary',
-    author='Espec North America',
-    author_email='mmetzler@espec.com',
+    author='Paul Nong-Laolam',
+    author_email='pnong-laolam@espec.com',
     license='MIT',
     packages=['chamberconnectlibrary'],
-    install_requires=['pyserial'],
+    install_requires=['pyserial', 'minimalmodbus'],
     zip_safe=False,
-    keywords='Espec P300 SCP220 ES102 F4T F4',
+    keywords='Espec P300 P300vib SCP220 ES102 F4T F4',
     include_package_data=True,
-    scripts=['bin/chamberconnectlibrary-test.py']
+    scripts=['bin/f4t_runTCP.py', 'bin/p300vib_sample_run.py'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.7.3',
+        'Programming Language :: Python :: 3.8.10',
+        'Programming Language :: Python :: 3.9.3',
+        'Programming Language :: Python :: 3.10.12',
+    ],
 )
